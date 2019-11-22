1be3e81d0f56dcea5eec5d25d41a2953cca801f4
kat <kat.lilly@gmail.com>
2019-11-22 13:54:52 +1300

    multithreaded decoding not yet faster than single threaded

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ serialise : write_compressed_lists.cpp pack512.cpp
 	g++ -Wall -march=native -std=c++11 pack512.cpp write_compressed_lists.cpp -o serialise
 
 multithread_decompress : multithreaded_decompress_lists.cpp pack512.cpp
-	g++ -Wall -march=native -std=c++11 pack512.cpp multithreaded_decompress_lists.cpp -o multithread_decompress
+	g++ -Wall -march=native -std=c++11 pack512.cpp multithreaded_decompress_lists.cpp -o multithread_decompress -lpthread
 
 
 clean:
--- multithreaded_decompress_lists.cpp
+++ multithreaded_decompress_lists.cpp
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <stdint.h>
+#include <thread>
+#include <atomic>
+#include <vector>
 #include "pack512.h"
 
+//#define DEBUG 
 
 class list_data
 	{
@@ -18,6 +22,56 @@ public:
 		//nothing
 		}
 	};
+
+class Flag
+	{
+public:
+	std::atomic <int> flag;
+
+	Flag() : flag(0) {}
+	};
+
+
+void thread_decode(std::vector<Flag> &flags, uint8_t *compressedpostings, int *pointers, list_data *metadata)
+	{
+	// instatiate a compressor to decode with and allocate memory to decode into
+	Pack512 whakanui;
+	int *decoded = new int[10000];
+	uint8_t *payload;
+	uint8_t *selectors;
+	
+	for (int i = 0; i < flags.size(); i++)
+		if (flags[i].flag == 0)
+			{
+			int expected = 0;
+			if (!flags[i].flag.compare_exchange_strong(expected, 1))
+				continue;
+//			printf("decoding list number %d\n", i);
+
+			int listlength = metadata[i].length;
+			int payload_bytes = metadata[i].payload_bytes;
+			int numselectors = metadata[i].selector_bytes;
+//			printf("length: %d, payload bytes: %d, selector bytes: %d\n",
+//				listlength, payload_bytes, numselectors);
+			
+			// point to compressed payload and selectors
+			payload = compressedpostings + pointers[i];
+			selectors = compressedpostings + pointers[i] + payload_bytes;
+			
+			int num_decompressed = whakanui.decompress(decoded, selectors, numselectors,
+				(int *) payload, listlength);
+			
+//			printf("decoding list number %d, decompressed %d of %d dgaps\n", i, num_decompressed, listlength);
+
+//			if (i == 1)
+//				for (int j = 0; j < listlength; j++)
+//					printf("%d, ", decoded[j]);
+			
+
+			
+			}
+
+	}
 
 
 int main(void)
@@ -71,6 +125,27 @@ int main(void)
 
 
 	/*
+	  Set up flags and threads
+	 */
+	std::vector<std::thread> decoder_pool;
+	std::vector<Flag> done(num_elements);
+	int num_threads = 16;
+	for (int i = 0; i < num_threads; i++)
+		decoder_pool.push_back(std::thread(thread_decode, std::ref(done), compressedpostings, pointers, metadata));
+
+
+   /*
+	  Set up timers and start threads
+	 */
+	auto start = std::chrono::steady_clock::now();
+	for (auto &decoder : decoder_pool)
+		decoder.join();
+	auto finish = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed = finish - start;
+	double time = elapsed.count();
+	printf("%5f\n", time);
+	
+	/*
 	  Decompress list #1
 	*/
 	int i = 1;
@@ -84,21 +159,20 @@ int main(void)
 	Pack512 whakanui;
 	//allocate memory to decode into
 	int *decoded = new int[listlength];
-	// pointer to payload
+	// point to compressed payload and selectors
 	uint8_t *payload = compressedpostings + pointers[i];
-	// pointer to selectors
 	uint8_t *selectors = compressedpostings + pointers[i] + payload_bytes;
-//	for (int j = 0; j < numselectors; j++)
-//		printf("%d, ", selectors[j]);
-//	printf("\n");
 	
-	int num_decompressed = whakanui.decompress(decoded, selectors, numselectors, (int *) payload, listlength);
+	int num_decompressed = whakanui.decompress(decoded, selectors, numselectors,
+		(int *) payload, listlength);
 
 	printf("decompressed %d dgaps\n", num_decompressed);
 
+	#ifdef DEBUG
 	for (int i = 0; i < num_decompressed; i++)
 		printf("%d, ", decoded[i]);
 	printf("\n");
+	#endif
 	
 	return 0;
 	}
